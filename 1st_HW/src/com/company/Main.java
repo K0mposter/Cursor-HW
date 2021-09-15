53b34b3df80c01209421113be877b311f258b354
K0mposter <Oleksiisubbotin@gmail.com>
2021-09-15 16:10:33 +0300

    first commit

---
+++ 1st_HW/src/com/company/Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int num1 = 15, num2 = 7, sum = 0;
+        System.out.println(sum = num1 + num2);
+        System.out.println(sum = num1 - num2);
+        System.out.println(sum = num1 * num2);
+        System.out.println(sum = num1 / num2);
+    }
+}
+class Byte {
+
+    public static void main(String[] args) {
+        byte num1 = 120, num2 = 10, sum = 0;
+        System.out.println(sum = (byte) (num1 + num2));
+        System.out.println(sum = (byte) (num1 - num2));
+        System.out.println(sum = (byte) (num1 * num2));
+        System.out.println(sum = (byte) (num1 / num2));
+    }
+}
+class Short {
+
+    public static void main(String[] args) {
+        short num1 = 705, num2 = 150, sum = 0;
+        System.out.println(sum = (short) (num1 + num2));
+        System.out.println(sum = (short) (num1 - num2));
+        System.out.println(sum = (short) (num1 * num2));
+        System.out.println(sum = (short) (num1 / num2));
+    }
+}
+class Double {
+
+    public static void main(String[] args) {
+        double num1 = 75.5D, num2 = 18.9D, sum = 0;
+        System.out.println(sum = (double) (num1 + num2));
+        System.out.println(sum = (double) (num1 - num2));
+        System.out.println(sum = (double) (num1 * num2));
+        System.out.println(sum = (double) (num1 / num2));
+    }
+}
+class Float {
+
+    public static void main(String[] args) {
+        float num1 = 46.6F, num2 = 12.5F, sum = 0;
+        System.out.println(sum = (float) (num1 + num2));
+        System.out.println(sum = (float) (num1 - num2));
+        System.out.println(sum = (float) (num1 * num2));
+        System.out.println(sum = (float) (num1 / num2));
+    }
+}
