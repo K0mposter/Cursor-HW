833f42162ad820cc1bd1638127f91a2494ce2301
K0mposter <Oleksiisubbotin@gmail.com>
2021-09-15 19:24:38 +0300

    1st HW added

---
+++ Cursor_HW1/src/com/company/Main.java
@@ -0,0 +1,52 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num1 = 15, num2 = 7;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+    }
+}
+class Byte {
+
+    public static void main(String[] args) {
+        byte num1 = 120, num2 = 10;
+        System.out.println((byte) (num1 + num2));
+        System.out.println((byte) (num1 - num2));
+        System.out.println((byte) (num1 * num2));
+        System.out.println((byte) (num1 / num2));
+    }
+}
+class Short {
+
+    public static void main(String[] args) {
+        short num1 = 705, num2 = 150;
+        System.out.println((short) (num1 + num2));
+        System.out.println((short) (num1 - num2));
+        System.out.println((short) (num1 * num2));
+        System.out.println((short) (num1 / num2));
+    }
+}
+class Double {
+
+    public static void main(String[] args) {
+        double num1 = 75.5D, num2 = 18.9D;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+    }
+}
+class Float {
+
+    public static void main(String[] args) {
+        float num1 = 56.6F, num2 = 12.7F;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+    }
+}
