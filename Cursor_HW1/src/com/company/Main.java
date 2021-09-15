d80a01ad8effd419465247a497145ae4e9a7d445
K0mposter <Oleksiisubbotin@gmail.com>
2021-09-15 20:01:52 +0300

    Refactored

--- Cursor_HW1/src/com/company/Main.java
+++ Cursor_HW1/src/com/company/Main.java
@@ -4,49 +4,74 @@ public class Main {
 
     public static void main(String[] args) {
         int num1 = 15, num2 = 7;
-        System.out.println(num1 + num2);
-        System.out.println(num1 - num2);
-        System.out.println(num1 * num2);
-        System.out.println(num1 / num2);
+        int Add, Ded, Mult, Div;
+        Add = num1 + num2;
+        Ded = num1 - num2;
+        Mult = num1 * num2;
+        Div = num1 / num2;
+        System.out.println(Add);
+        System.out.println(Ded);
+        System.out.println(Mult);
+        System.out.println(Div);
     }
 }
 class Byte {
 
     public static void main(String[] args) {
         byte num1 = 120, num2 = 10;
-        System.out.println((byte) (num1 + num2));
-        System.out.println((byte) (num1 - num2));
-        System.out.println((byte) (num1 * num2));
-        System.out.println((byte) (num1 / num2));
+        byte Add, Ded, Mult, Div;
+        Add = (byte) (num1 + num2);
+        Ded = (byte) (num1 - num2);
+        Mult = (byte) (num1 * num2);
+        Div = (byte) (num1 / num2);
+        System.out.println(Add);
+        System.out.println(Ded);
+        System.out.println(Mult);
+        System.out.println(Div);
     }
 }
 class Short {
 
     public static void main(String[] args) {
         short num1 = 705, num2 = 150;
-        System.out.println((short) (num1 + num2));
-        System.out.println((short) (num1 - num2));
-        System.out.println((short) (num1 * num2));
-        System.out.println((short) (num1 / num2));
+        short Add, Ded, Mult, Div;
+        Add = (short) (num1 + num2);
+        Ded = (short) (num1 - num2);
+        Mult = (short) (num1 * num2);
+        Div = (short) (num1 / num2);
+        System.out.println(Add);
+        System.out.println(Ded);
+        System.out.println(Mult);
+        System.out.println(Div);
     }
 }
 class Double {
 
     public static void main(String[] args) {
         double num1 = 75.5D, num2 = 18.9D;
-        System.out.println(num1 + num2);
-        System.out.println(num1 - num2);
-        System.out.println(num1 * num2);
-        System.out.println(num1 / num2);
+        double Add, Ded, Mult, Div;
+        Add = num1 + num2;
+        Ded = num1 - num2;
+        Mult = num1 * num2;
+        Div = num1 / num2;
+        System.out.println(Add);
+        System.out.println(Ded);
+        System.out.println(Mult);
+        System.out.println(Div);
     }
 }
 class Float {
 
     public static void main(String[] args) {
         float num1 = 56.6F, num2 = 12.7F;
-        System.out.println(num1 + num2);
-        System.out.println(num1 - num2);
-        System.out.println(num1 * num2);
-        System.out.println(num1 / num2);
+        float Add, Ded, Mult, Div;
+        Add = num1 + num2;
+        Ded = num1 - num2;
+        Mult = num1 * num2;
+        Div = num1 / num2;
+        System.out.println(Add);
+        System.out.println(Ded);
+        System.out.println(Mult);
+        System.out.println(Div);
     }
 }
