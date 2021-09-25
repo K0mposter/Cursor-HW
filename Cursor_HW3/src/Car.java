5e2f33373e55215c26d31fcc1dca71364b89385f
K0mposter <OleksiiSubbotin@gmail.com>
2021-09-25 16:35:39 +0300

    tupnyaki

--- Cursor_HW3/src/Car.java
+++ Cursor_HW3/src/Car.java
@@ -1,36 +1,54 @@
-import java.util.Scanner;
 
 public class Car {
     public static void main(String[] args) {
-        Car myCar = new Car();
-        Scanner condition = new Scanner(System.in);
+        Vehicle myCar = new Vehicle();
 
-        System.out.println("Enter speed");
-        int CurrentSpeed = condition.nextInt();
+        System.out.println("status= " +myCar.engineStatus);
+        System.out.println("speed= " + myCar.currentSpeed);
+        System.out.println("............................");
         myCar.Start();
-
-
+        System.out.println("status= " +myCar.engineStatus);
+        System.out.println("speed= " + myCar.currentSpeed);
 
 }
- static class Vehicle {
-     private boolean EngineStatus = false;
-     private double CurrentSpeed = 0;
+  class Vehicle {
+     private boolean engineStatus = false;
+     private double currentSpeed = 0;
 
-     public void Start() {
-         EngineStatus = true;
-             System.out.println("Engine online");
+     public Vehicle() {
+     }
+
+     public Vehicle(boolean engineStatus, double currentSpeed) {
+         this.engineStatus = engineStatus;
+         this.currentSpeed = currentSpeed;
+     }
+
+     public void ChangeSpeed(double currentSpeed){
+         this.currentSpeed = currentSpeed;
      }
      public void Stop(){
-        EngineStatus = false;
-         System.out.println("Engine offline");
+         currentSpeed =0;
+         engineStatus = false;
+     }
+     public void Start(){
+         currentSpeed =0;
+         engineStatus = true;
+     }
+
+     public boolean isEngineStatus() {
+         return engineStatus;
+     }
+
+     public void setEngineStatus(boolean engineStatus) {
+         this.engineStatus = engineStatus;
+     }
+
+     public double getCurrentSpeed() {
+         return currentSpeed;
+     }
+
+     public void setCurrentSpeed(double currentSpeed) {
+         this.currentSpeed = currentSpeed;
      }
  }
-
-     public void Drive(int Speed) {
-         if (Speed > 0 && EngineStatus = true)
-             System.out.println("Car driving with " + Speed + " kilometers per hour ");
-         else {
-             System.out.println("Engine offline");
-         }
-     }
  }
