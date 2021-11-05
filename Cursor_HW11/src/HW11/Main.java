6342a022de3ec8996368a8500a59214ae3db352c
K0mposter <OleksiiSubbotin@gmail.com>
2021-11-05 19:22:39 +0200

    IO fix

--- Cursor_HW11/src/HW11/Main.java
+++ Cursor_HW11/src/HW11/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader input = new BufferedReader(new FileReader("src/HW11/ToCopy.txt"));
-        BufferedWriter output = new BufferedWriter(new FileWriter("src/HW11/Copied.txt"));
+        BufferedReader input = new BufferedReader(new FileReader("Cursor_HW11/src/HW11/ToCopy.txt"));
+        BufferedWriter output = new BufferedWriter(new FileWriter("Cursor_HW11/src/HW11/Copied.txt"));
 
         String string;
         while ((string = input.readLine()) != null) {
